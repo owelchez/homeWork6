4237fc02c1e7eda723fa58f209af221297fb7f5b
Orvin Welchez <owelchez@CRESTRON.CRESTRON.COM>
2016-06-20 19:54:32 -0400

    Move Files

---
+++ homeWork6.js
@@ -0,0 +1,27 @@
+$('.animalContainer').on('click', function(){
+	
+	
+
+
+			var animal = $(this).data('animals');
+			var queryURL = "http://api.giphy.com/v1/gifs/search?q=";
+			var apiKey = 'dc6zaTOxFJmzC';
+
+// Onclick event handler here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
