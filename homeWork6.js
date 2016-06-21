e14996826737a32953190d427f5e6bccf2c06837
Orvin Welchez <owelchez@CRESTRON.CRESTRON.COM>
2016-06-21 16:35:31 -0400

    Nothing

--- homeWork6.js
+++ homeWork6.js
@@ -1,30 +1,28 @@
+	var apiKey = 'dc6zaTOxFJmzC';
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";	
+
+	
+	$.ajax({
+		url: queryURL,
+		method: 'GET',
+	}).done(function(response){		
+
+
+
+	/*Creates buttons for animals and adds them to the HTML*/
 
 $('#animalForm').on('submit', function () {
 			var animalInput = $('#animalInput').val();
-			console.log('You\'re trying to search for: ', animalInput);
-			var animalDiv = $('<div>');
-			var pTag = $('<p>', {
-				text: animalInput,
-			})
+			var animalButton = $('<button>', {
+				id: 'animal',
+			});
+			$(animalButton).append(animalInput);
+			$('.animalContainer').append(animalButton);
 			return false;
 		});
 
 
 
-	
-				
-
-	/*var animal = $(this).data('animal');	
-	var apiKey = 'dc6zaTOxFJmzC';
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";											
-
-	$.ajax({
-		url: queryURL,
-		method: 'GET',
-	}).done(function(response){
-
 		console.log(response);
-	});*/
 
-
-
+}); // Ajax function response closing braces
